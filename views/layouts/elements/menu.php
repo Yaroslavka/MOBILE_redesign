df18c081b76767a142464f391f3a3c2bbff15300
nahkar <dimabelenov@gmail.com>
2014-12-11 12:26:17 +0200

    Меню боковое

--- views/layouts/elements/menu.php
+++ views/layouts/elements/menu.php
@@ -2,9 +2,14 @@
     <ul>
         <li><a href="<?php echo Yii::app()->createUrl('/site/project');?>">О проекте</a></li>
         <li class="clinic_menu"><a href="<?php echo Yii::app()->createUrl('/clinic/index');?>">Все клиники</a></li>
+        <li class="network_clinic_menu"><a href="<?php echo Yii::app()->createUrl('/clinic/index');?>">Сетевые клиники</a></li>
         <li  class="doctor_menu"><a href="<?php echo Yii::app()->createUrl('/doctor/index');?>">Все врачи</a></li>
-        <!--<li><a href="<?php // echo Yii::app()->createUrl('/site/blogList');?>">Блоги</a></li>-->
-        <!--<li><a href="<?php // echo Yii::app()->createUrl('/site/serviceList');?>">Медицинские услуги</a></li>-->
-        <!--<li><a href="<?php // echo Yii::app()->createUrl('/site/illnessList');?>">Справочник заболеваний</a></li>-->
+        <li><a href="<?php  echo Yii::app()->createUrl('/site/serviceList');?>">Медицинские услуги</a></li>
+        <li><a href="<?php  echo Yii::app()->createUrl('/site/blogList');?>">Блоги</a></li>
+        <li><a href="<?php  echo Yii::app()->createUrl('/site/illnessList');?>">Справочник заболеваний</a></li>
+        <li class="bottom_menu">
+          <div class="top"><a class="more_url" href="http://medbooking.com/"><span>Перейти к основной версии сайта</span></a></div>
+          <div class="bottom">&copy; Сopyright 2013 Medbooking.com.<br>Все права защищены.</div>
+        </li>
     </ul>
 </div>
